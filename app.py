da11a89a48116a70c699db57fe7a22c0a9fb74ee
Admin <verysbond@ya.ru>
2024-01-06 17:48:23 +0300

    flaskBlog files

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+@app.route('/about')
+def about():
+    return 'About us'
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User page' + name + "-" +id
+
+if __name__ == '__main__':
+    app.run(debug=True)
