8b7980179c8c49b0e2aadd28d128b8068bfa85e6
Admin <verysbond@ya.ru>
2024-01-07 16:11:03 +0300

    flaskBlog files

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'hello world'
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
-    return 'About us'
+    return render_template("about.html")
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
